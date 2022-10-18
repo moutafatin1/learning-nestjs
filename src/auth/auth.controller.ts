ca2afef5ccb3d78bae9cbfe2a941731038d72599
Oussama Moutafatin <moutafatin3@gmail.com>
2022-10-18 08:58:08 +0100

    test  db

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
 export class AuthController {
-  @Get('aza')
-  getUser() {
-    return 'Hello User';
+  constructor(private authService: AuthService) {}
+
+  @Get('users')
+  async getUser() {
+    const users = await this.authService.getUser();
+    return users;
   }
 }
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,11 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(private prisma: PrismaService) {}
+
+  getUser() {
+    return this.prisma.user.findMany();
+  }
+}
